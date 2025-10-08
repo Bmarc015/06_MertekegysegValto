3e15b5fcf39378637a4b146719f93152c62db4a3
Marta Barcell <bartamarcell0@gmail.com>
2025-10-08 10:26:34 +0200

    kesz hosszvalto

--- view/404.php
+++ view/404.php
@@ -10,7 +10,7 @@
 <body>
     <h1>404</h1>
     <p><a href="<?php echo $mappa . "/valutavalto" ?>">Pénzváltó</a></p>
-    <p><a href="<?php echo $mappa . "/valutavalto" ?>">Mertekegységváltó</a></p>
+    <p><a href="<?php echo $mappa . "/mertekegysegvalto" ?>">Mertekegységváltó</a></p>
 
     <img src="public/kep.jfif" alt="">
 </body>
